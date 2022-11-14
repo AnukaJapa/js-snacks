60f7f8233b888a2e88280572e748a883d70ac731
AnukaJapa <japaridze.ana96@gmail.com>
2022-11-14 16:50:58 +0100

    inserisci nell'array solo i numeri dispari

--- jsnack5/script.js
+++ jsnack5/script.js
@@ -4,6 +4,7 @@ let array = [];
 for(let i=0; i<6; i++){
 
 let newNumber = prompt("inserisci il numero");
+//verificare se quello inserito è il numero
 for(;;){
     if(isNaN(newNumber) || newNumber == "" || newNumber==" "){       
     newNumber = prompt("inserisci il NUMERO!");
@@ -11,8 +12,10 @@ for(;;){
 break;
 }
 }
-
-array.push(newNumber);
+//verifica se il numero è dispari e se è inseriscilo nell'array
+if(newNumber%2!=0){
+    array.push(newNumber);
+}
 }
 
 // stampare l'array nel console;
