3d73f9b1dc41d77e09ba423a074f5d41ed761b35
AnukaJapa <japaridze.ana96@gmail.com>
2022-11-14 17:06:22 +0100

    far determinare all'user massimo numero per random

--- jsnack6/script.js
+++ jsnack6/script.js
@@ -1,9 +1,20 @@
-let random = Math.floor(Math.random()*10);
+let numeroMassimo = prompt("determina il numero massimo fino a cui estrarre");
+//verifica se il numero inserito è valido
+for(;;){
+    if(isNaN(numeroMassimo) || numeroMassimo=="" || numeroMassimo==" "){
+        numeroMassimo = prompt("isnerisci il NUMERO!");
+    }else{
+break;
+    }
+}
+
+let random = Math.floor(Math.random()*numeroMassimo);
 let userNumber = prompt("inserisci il tuo numero");
+
 
 //verificare se il numeor inserito è valido
 for(;;){
-    if(isNaN(userNumber)|| userNumber=="" || userNumber==" "){
+    if(isNaN(userNumber) || userNumber=="" || userNumber==" "){
         userNumber = prompt("isnerisci il NUMERO!");
     }else{
 break;
@@ -12,7 +23,7 @@ break;
 
 //verificare se il numero di user è uguale con il numero random
 if(random == userNumber){
-    console.log("hai vinto!");
+    console.log(`numero random è ${random}; \n il tuo numero è: ${userNumber} \n quindi hai vinto!`);
 }else {
-    console.log("hai perso!");
+    console.log(`numero random è ${random}; \n il tuo numero è: ${userNumber} \n quindi hai perso!`);
 }
