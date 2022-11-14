bc88bde177d18810917a8b8fd944bc881335fb14
AnukaJapa <japaridze.ana96@gmail.com>
2022-11-14 17:10:13 +0100

    modifica

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -5,14 +5,14 @@
 
 for(let i = 0; i<2;){
 //verificare se il primo numero inserito è il numero e solo dopo verifica passa a richiedere il secondo numero
-if(isNaN(firstNumber) || firstNumber == ""){
+if(isNaN(firstNumber) || firstNumber == "" || firstNumber == " "){
     console.log("il numero inserito non è numero prova ancora");
     firstNumber = prompt("inserisci il primo NUMERO");
 } else {
     secondNumber = prompt("inserisci il secondo numero");
     
     for(let j = 0; j<2;) {
-        if(isNaN(secondNumber) || secondNumber == ""){
+        if(isNaN(secondNumber) || secondNumber == "" || firstNumber == " "){
             console.log("il numero inserito non è numero prova ancora");
             secondNumber = prompt("inserisci il secondo NUMERO");
         } else {
