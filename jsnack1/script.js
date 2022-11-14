5637974a70e29abb6aef157ecaacac8bdeacab24
AnukaJapa <japaridze.ana96@gmail.com>
2022-11-14 14:01:00 +0100

    verificare se quello inserito dall'utente è numero

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,7 +1,29 @@
-let firstNumber = prompt("inserisci il primo numero");
-let secondNumber = prompt("inserisci il secondo numero");
+
+ let firstNumber = prompt("inserisci il primo numero");
+ let secondNumber;
 
 
+for(let i = 0; i<2;){
+//verificare se il primo numero inserito è il numero e solo dopo verifica passa a richiedere il secondo numero
+if(isNaN(firstNumber) || firstNumber == ""){
+    console.log("il numero inserito non è numero prova ancora");
+    firstNumber = prompt("inserisci il primo NUMERO");
+} else {
+    secondNumber = prompt("inserisci il secondo numero");
+    
+    for(let j = 0; j<2;) {
+        if(isNaN(secondNumber) || secondNumber == ""){
+            console.log("il numero inserito non è numero prova ancora");
+            secondNumber = prompt("inserisci il secondo NUMERO");
+        } else {
+            j=2; //break;
+            i=2;
+        } 
+    }
+} 
+}
+
+//verificare se l'utente ha inserito il numero uguale
 
 if(firstNumber != secondNumber){
     let maxNumber = Math.max(firstNumber,secondNumber);
