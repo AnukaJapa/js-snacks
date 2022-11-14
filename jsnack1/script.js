559bef42126e6a70a75570bf9ffa5266770b33e3
AnukaJapa <japaridze.ana96@gmail.com>
2022-11-14 13:15:04 +0100

    comparare i due numeri

---
+++ jsnack1/script.js
@@ -0,0 +1,19 @@
+let firstNumber = prompt("inserisci il primo numero");
+let secondNumber = prompt("inserisci il secondo numero");
+
+
+
+if(firstNumber != secondNumber){
+    let maxNumber = Math.max(firstNumber,secondNumber);
+    console.log(`Questo è il massimo numero da te inserito: ${maxNumber}`);
+} else {
+    console.log("i numeri tu hai inserito sono uguali");
+}
+
+
+
+
+
+
+
+
